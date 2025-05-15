e3e47a9867046bfb87c24085163e4784bbb3fa87
durhavazla <durhavazla@gmail.com>
2025-05-15 15:32:22 +0700

    Update app.vue

--- app.vue
+++ app.vue
@@ -1,12 +1,37 @@
 <script setup>
+useSeoMeta({
+  robots: 'index, follow',
+  title: 'KILAT289 - Situs Gaming Terlengkap Dengan Ragam Pilihan Terbaik 2025',
+  ogTitle: 'KILAT289 - Situs Gaming Terlengkap Dengan Ragam Pilihan Terbaik 2025',
+  description: 'KILAT289 merupakan situs gaming terlengkap dengn beragan jenis games online terbaik 2025 yang juga memberikan layanan cepat dan ramah serta memiliki winrate gaming paling tinggi saat ini diseluruh Indonesia.',
+  ogDescription: 'KILAT289 merupakan situs gaming terlengkap dengn beragan jenis games online terbaik 2025 yang juga memberikan layanan cepat dan ramah serta memiliki winrate gaming paling tinggi saat ini diseluruh Indonesia.',
+  ogImage: 'hero.jpg',
+  twitterCard: 'summary_large_image',
+      })
+
 useHead({
   htmlAttrs: { lang: "id-ID" },
   title: "KILAT289 - Situs Gaming Terlengkap Dengan Ragam Pilihan Terbaik 2025",
+  link: [
+    {
+      rel: 'canonical',
+      href: 'https://kilat289.netlify.app'
+    },
+    {
+      rel: 'amphtml',
+      href: 'https://kilat289.netlify.app/amp'
+    }
+  ],
   meta: [
     {
       name: "description",
       content:
         "KILAT289 merupakan situs gaming terlengkap dengn beragan jenis games online terbaik 2025 yang juga memberikan layanan cepat dan ramah serta memiliki winrate gaming paling tinggi saat ini diseluruh Indonesia.",
+    },
+    {
+      name: "google-site-verification",
+      content:
+        "BqonHV6y--MgXz8KSikW2p-g4psEplTDLYYUxpEJ0qc.",
     },
   ],
   bodyAttrs: {
